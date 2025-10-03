4a5c5683596e40c626635dba6a5ee30099de4047
noorjahan <noorjahanshaik234@gmail.com>
2025-10-03 23:53:00 +0530

    Initial commit pulse assignment

---
+++ pulse-assignment/src/main/java/com/noorjahan/pulseassignment/PulseAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.noorjahan.pulseassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseAssignmentApplication.class, args);
+	}
+
+}
---
+++ pulse-assignment/src/test/java/com/noorjahan/pulseassignment/PulseAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noorjahan.pulseassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
